7e7a42484528cb143af9b44156233c591149401d
Luis Acuna <leacunavega@gmail.com>
2022-01-24 07:35:11 +0100

    ADD

--- schema.sql
+++ schema.sql
@@ -8,3 +8,28 @@ name TEXT,
 PRIMARY KEY (id)
 );
 INSERT INTO user values(111122,'Luis Acuña');
+
+CREATE TABLE questions
+(
+id INTEGER AUTO_INCREMENT,
+name TEXT,
+intent TEXT,
+PRIMARY KEY (id)
+);
+
+INSERT INTO questions values(1,'question1', '¿Qué día de la semana es');
+INSERT INTO questions values(2,'question2', '¿Qué fecha es hoy?');
+INSERT INTO questions values(3,'question3', '¿Qué edad tienes?');
+INSERT INTO questions values(4,'game1', '¿Cuanto es la suma entre 11 y 15?');
+INSERT INTO questions values(5,'game2', 'Si la vocal "a" es la posición 1 y la "u" es la 5, qué posición es la "i"?');
+
+CREATE TABLE expected_results
+(
+user_id TEXT,
+question_id TEXT,
+answer TEXT,
+);
+
+INSERT INTO expected_results values("111122", "question3", "37");
+
+
